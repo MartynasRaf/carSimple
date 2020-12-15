e9b4039cc796ee21f65a40bafaba4fc11edc2bf4
Marius Šomka <marius.somka@mif.stud.vu.lt>
2020-12-16 01:51:50 +0200

    better info layout for detailed screen

--- src/Screens/DetailedCarScreen.js
+++ src/Screens/DetailedCarScreen.js
@@ -6,14 +6,14 @@ import {
 	Image,
 	Dimensions,
 	TouchableOpacity,
+	ScrollView,
 } from 'react-native';
 import AdjustLabel from '../Components/AdjustLabel';
 import { FontAwesome } from '@expo/vector-icons';
-import { StatusBar } from 'react-native';
 
 const DetaileCarScreen = ( {navigation} ) => {
 	return (
-			<View>
+			<ScrollView>
 				<TouchableOpacity style={styles.favoriteButton}>
 					<FontAwesome
 						name='heart'
@@ -35,45 +35,95 @@ const DetaileCarScreen = ( {navigation} ) => {
 						style={styles.image}
 					/>
 				<View style={{zIndex: 3}}>
+					<View>
+						<AdjustLabel
+							fontSize={15}
+							text='1998'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={30}
+							text='Mercedes-Benz'
+							style={styles.textHeader}
+							numberOfLines={1}
+						/>
+					</View>
+						<AdjustLabel
+							fontSize={20}
+							text='Main Details:'
+							style={styles.textSubHeader}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='1.0l SUV'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='Diesel'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='Hybrid'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='Used (mileage: 10 000 km)'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='Gearbox: automatic'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={20}
+							text='Additional Details: '
+							style={styles.textSubHeader}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='Steering Wheel Position: Left'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='Drive wheels: Front'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
+						<AdjustLabel
+							fontSize={15}
+							text='Defects: none'
+							style={styles.textSimple}
+							numberOfLines={1}
+						/>
 					<AdjustLabel
-						fontSize={15}
-						text='1998'
-						style={styles.textSimple}
-						numberOfLines={1}
-					/>
-					<AdjustLabel
-						fontSize={25}
-						text='Mercedes-Benz'
-						style={styles.textHeader}
-						numberOfLines={1}
-					/>
-
-					<AdjustLabel
-						fontSize={15}
-						text='1.0l SUV'
-						style={styles.textSimple}
-						numberOfLines={1}
-					/>
-
-					<AdjustLabel
-						fontSize={15}
-						text='Diesel'
-						style={styles.textSimple}
-						numberOfLines={1}
-					/>
-
-					<AdjustLabel
-						fontSize={25}
+						fontSize={30}
 						text='19 500€'
 						style={styles.textPrice}
 						//numberOfLines={1}
 					/>
 				</View>
-			</View>
+			</ScrollView>
 	);
 };
 
 const styles = StyleSheet.create({
+	contentContainer: {
+		paddingBottom: 50
+	},
 	canvas: {
 		position: 'absolute',
 		marginTop: Dimensions.get('window').height / 3 - 25,
@@ -89,26 +139,33 @@ const styles = StyleSheet.create({
 		height: Dimensions.get('window').height / 3,
 		width: Dimensions.get('window').width,
 	},
-	textHeader: {
-		//position: 'absolute',
+	textSubHeader:{
 		color: 'black',
 		marginHorizontal: 10,
-		marginBottom: 20,
+		marginTop: 20,
+		marginBottom: 5,
+		width: Dimensions.get('window').width,
+		fontWeight: 'bold',
+	},
+	textHeader: {
+		color: 'black',
+		marginHorizontal: 10,
 		width: Dimensions.get('window').width,
 		fontWeight: 'bold',
 	},
 	textSimple: {
 		color: 'black',
 		marginHorizontal: 10,
+		marginTop: 5,
 		//height: 20,
-		width: Dimensions.get('window').width / 2.5,
+		//width: Dimensions.get('window').width / 2.5,
 		fontWeight: '500',
 	},
 	textPrice: {
 		position: 'absolute',
 		textAlign: 'right',
 		color: 'grey',
-		//marginTop: 10,
+		marginTop: 10,
 		width: Dimensions.get('window').width - 20,
 		fontWeight: 'bold',
 	},
